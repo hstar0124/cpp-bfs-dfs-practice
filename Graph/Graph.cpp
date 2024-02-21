d38dd9925064838b27d8409a403166a8ea538a47
hstar0124 <dunjun10@naver.com>
2024-02-21 16:47:38 +0900

    add Dijikstra

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -232,12 +232,95 @@ void BfsAll()
 }
 
 
+// ========================================================================
+// Dijikstra (다익스트라) = BFS + 양념
+// Priority_queue
+
+void CreateGraphForDijikstra()
+{
+    vertices.resize(6);
+
+    adjacent = vector<vector<int>>(6, vector<int>(6, -1));
+    adjacent[0][1] = adjacent[1][0] = 15;
+    adjacent[0][3] = adjacent[3][0] = 35;
+    adjacent[1][2] = adjacent[2][1] = 5;
+    adjacent[1][3] = adjacent[3][1] = 10;
+    adjacent[3][4] = adjacent[4][3] = 5;
+    adjacent[5][4] = adjacent[4][5] = 5;
+
+}
+
+struct VertexCost
+{
+    VertexCost(int cost, int vertex) : cost(cost), vertex(vertex) {}
+
+    bool operator<(const VertexCost& other) const
+    {
+        return cost < other.cost;
+    }
+
+    bool operator>(const VertexCost& other) const
+    {
+        return cost > other.cost;
+    }
+
+    int cost;
+    int vertex;
+};
+
+void Dijikstra(int here)
+{
+    priority_queue<VertexCost, vector<VertexCost>, greater<VertexCost>> pq;
+    vector<int> best(6, INT32_MAX);
+    vector<int> parent(6, -1);
+
+    pq.push(VertexCost(0, here));
+    best[here] = 0;
+    parent[here] = here;
+
+    while (pq.empty() == false)
+    {
+        VertexCost v = pq.top();
+        pq.pop();
+
+        int cost = v.cost;
+        here = v.vertex;
+
+        if (best[here] < cost)
+            continue;
+
+        cout << "방문!" << here << endl;
+
+        for (int there = 0; there < 6; there++)
+        {
+            if (adjacent[here][there] == -1)
+                continue;
+
+            int nextCost = best[here] + adjacent[here][there];
+            if (nextCost >= best[there])
+                continue;
+
+            best[there] = nextCost;
+            parent[there] = here;
+
+            pq.push(VertexCost(nextCost, there));
+        }
+    }
+
+}
+
+
+
 int main()
 {
-    CreateGraph();
-
+    /*CreateGraph();
     cout << "========== DFS ==========" << endl;
     DfsAll();
     cout << "========== BFS ==========" << endl;
-    BfsAll();
+    BfsAll();*/
+
+    cout << "========== Dijikstra ==========" << endl;
+    CreateGraphForDijikstra();
+    Dijikstra(0);
+
 }
